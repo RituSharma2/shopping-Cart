c705f2c55140b062b2d3c80496dcfb98fb948711
Pritesh Kumar <pritesh1908@gmail.com>
2021-11-25 13:53:06 +0530

    post request complete

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -47,5 +47,58 @@ const getDistrictsList = async function (req, res){
     }
 }
 
+const getByPin = async function (req, res){
+
+    try{ 
+
+        let pin= req.query.pincode
+        let date= req.query.date
+
+        let options = {
+          method : "get",
+          url : `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pin}&date=${date}`
+        }
+        let response= await axios(options)
+        
+
+
+        let centers= response.data
+        console.log(centers)
+        res.status(200).send( {msg: "Success", data: centers} )
+
+    }
+    catch(err) {
+        console.log(err.message)
+        res.status(500).send( { msg: "Something went wrong" } )
+    }
+}
+
+
+const getOtp = async function (req, res){
+
+    try{ 
+
+         let options = {
+          method : "post", // method has to be post
+          url : `https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP`,
+          data: { "mobile": req.body.mobile  } // we are sending the json body in the data 
+        }
+        let response= await axios(options)
+
+        let id= response.data
+        res.status(200).send( {msg: "Success", data: id} )
+
+    }
+    catch(err) {
+        console.log(err.message)
+        res.status(500).send( { msg: "Something went wrong" } )
+    }
+}
+
+
+
+
 module.exports.getStatesList = getStatesList;
 module.exports.getDistrictsList = getDistrictsList;
+module.exports.getByPin = getByPin;
+module.exports.getOtp = getOtp;
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 const cowinController= require("../controllers/cowinController")
 
-
 router.get("/cowin/states", cowinController.getStatesList)
 router.get("/cowin/districts/:stateId", cowinController.getDistrictsList)
+router.get("/cowin/centers", cowinController.getByPin)
+router.post("/cowin/getOtp", cowinController.getOtp)
 
 
 module.exports = router;
